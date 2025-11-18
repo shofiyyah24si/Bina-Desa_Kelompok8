2bfb39fc1aa94667e1fbf419eee2858f4efdec10
shofiyyah24si <shofiyyah24si@mahasiswa.pcr.ac.id>
2025-11-18 10:02:45 +0700

    update crud kejadian

--- app/Models/warga.php
+++ app/Models/warga.php
@@ -2,18 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Warga extends Model
 {
-    use HasFactory;
-
     protected $table = 'warga';
     protected $primaryKey = 'warga_id';
-    public $incrementing = true;   // kalau PK auto increment
-    protected $keyType = 'int';    // tipe data PK integerrr
-
     protected $fillable = [
         'no_ktp',
         'nama',
